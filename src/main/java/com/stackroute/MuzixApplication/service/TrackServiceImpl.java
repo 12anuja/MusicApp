b17629f414f2ab7ccb595258cf718b165bc1b8be
12anuja <51943001+12anuja@users.noreply.github.com>
2019-07-30 19:30:28 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/MuzixApplication/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/MuzixApplication/service/TrackServiceImpl.java
@@ -76,4 +76,39 @@ public class TrackServiceImpl implements TrackService {
     public List<Track> getTrackByNameOrComments(String name) {
         return trackRepository.findTrackByNameOrComments(name);
     }
+    
+    
+      //method to get tracks from api and save to database
+    @Override
+    public void saveTracksFromApi(){
+        //RestTemplate gets response from an api
+        RestTemplate restTemplate = new RestTemplate();
+        //url of Last.fm api
+        String fooResourceUrl = "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=081010bfa07bd3c5b56ee809b476993a&format=json";
+        //store response in a ResponseEntity
+        ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+
+        //Object Mapper to access the JSON from the response entity
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = null;
+        try {
+            //read the response body to get JSON object
+            root = mapper.readTree(response.getBody());
+            //Store the JSON array in the object to a variable
+            ArrayNode arrayNode = (ArrayNode) root.path("tracks").path("track");
+
+            //iterate the JSON array
+            for (int i = 0; i < arrayNode.size(); i++) {
+                //get a new Track object and fill it with data using setters
+                Track track = new Track();
+                track.setTrackName(arrayNode.get(i).path("name").asText());
+                track.setTrackComments(arrayNode.get(i).path("artist").path("name").asText());
+                //save the track to database
+                trackRepository.save(track);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
